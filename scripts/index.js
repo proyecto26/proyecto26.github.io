ef3316b9cb1b1e4438488a6328bc32c595f2d070
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2020-06-08 19:36:27 -0500

    Register service worker

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,13 @@
 (({ Worker, Waypoint }) => {
   window.worker = new Worker('/scripts/worker.js')
+  
+  // Check that service workers are supported
+  if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('/service-worker.js');
+    });
+  }
 
   window.delay = time => new Promise(resolve => setTimeout(() => resolve(), time))
 })(window)
