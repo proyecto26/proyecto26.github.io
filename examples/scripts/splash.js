4507bfc81f4d1684fade21ff263ad4cbca2d3b71
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2020-06-26 22:59:37 -0500

    Update splash.js

--- examples/scripts/splash.js
+++ examples/scripts/splash.js
@@ -5,11 +5,16 @@
     animatableSplash.removeEventListener('finish', animateLogo)
     await delay(300)
     worker.postMessage('splashEnd')
+    animatableSplash.remove()
   }
   animatableSplash.addEventListener('finish', animateLogo)
 
   function detectWokerEvent (e) {
     if (e.data === 'logoStart') {
+      animatableSplash.keyFrames = [
+        { offset: 0, transform: 'translateY(0)', opacity: 0 },
+        { offset: 1, transform: 'translateY(-100vh)', opacity: 1 }
+      ]
       animatableSplash.autoPlay = true
       worker.removeEventListener('message', detectWokerEvent)
     }
