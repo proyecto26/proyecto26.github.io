1a6df41ede9eb92316c7b973dd4c19514fdb27c8
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-03-28 22:55:09 -0500

    Request push notifications

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,13 @@
     window.addEventListener('load', function () {
       navigator.serviceWorker.register('/service-worker.js')
     })
+    
+    if ('Notification' in window) {
+      Notification.requestPermission(status => {
+        // status will either be 'default', 'granted' or 'denied'
+        console.log(`Notification permissions have been ${status}`)
+      })
+    }
   }
 
   window.delay = time => new Promise(resolve => setTimeout(() => resolve(), time))
